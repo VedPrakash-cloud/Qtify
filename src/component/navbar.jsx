09927982cc6a3766fad192a80ddd833cb1bef413
VedPrakash-cloud <rocky007ved@gmail.com>
2025-11-09 00:24:40 +0530

    input button size update4

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -95,7 +95,7 @@ export default function Navbar() {
               <input
                 type="text"
                 placeholder="Search an album of your choice"
-                className="w-auto p-2 rounded-l-md outline-none"
+                className="w-80 p-2 rounded-l-md outline-none"
               />
               <button type="button">
                 <CiSearch
