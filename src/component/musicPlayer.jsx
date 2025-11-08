f4444754ac7c2b029a06b423fdcf75b78cce174a
VedPrakash-cloud <rocky007ved@gmail.com>
2025-11-09 00:12:15 +0530

    volum button input button update

--- src/component/musicPlayer.jsx
+++ src/component/musicPlayer.jsx
@@ -57,7 +57,7 @@ export default function MusicPlayer({list}) {
           })}
       />
       </div>
-      <div className="flex w-1/5 items-center">
+      <div className="flex w-2/5 md:w-1/5 items-center">
         <VolumeDownRounded />
         <Slider
           aria-label="Volume"
